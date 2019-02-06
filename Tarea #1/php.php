9bd0548917c311aa2544071b0fa18c13ec51a96a
PrisCG <prisgudi17@hotmail.com>
2019-02-05 19:07:05 -0600

    Create php.php
    Ejercicio #10

---
+++ Tarea #1/php.php
@@ -0,0 +1,17 @@
+ <?php
+
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$nacimiento = $_POST['nacimiento'];
+$hm = $_POST['hm'];
+$direccion = $_POST['direccion'];
+$estudiante = $_POST['estudiante'];
+
+echo $nombre;
+echo $apellidos;
+echo $nacimiento;
+echo $hm;
+echo $direccion;
+echo $estudiante;
+
+?>
