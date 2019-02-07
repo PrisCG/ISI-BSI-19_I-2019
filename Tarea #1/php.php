e4fa761ea2fc9d54e8bfa6eab11613498541795f
Priscilla Cascante <47129579+PrisCG@users.noreply.github.com>
2019-02-06 20:00:36 -0600

    Update php.php

--- Tarea #1/php.php
+++ Tarea #1/php.php
@@ -6,12 +6,14 @@ $nacimiento = $_POST['nacimiento'];
 $hm = $_POST['hm'];
 $direccion = $_POST['direccion'];
 $estudiante = $_POST['estudiante'];
+$rango = $_POST['rango'];
 
-echo $nombre;
-echo $apellidos;
-echo $nacimiento;
-echo $hm;
-echo $direccion;
-echo $estudiante;
+echo "<b> El nombre es :   </b>". $_POST['nombre']. "<br />";
+echo "<b>Los apellidos son :  </b> ". $_POST['apellidos']. "<br />";
+echo "<b>El año de nacimiento es :  </b> ". $_POST['nacimiento']. "<br />";
+echo "<b>El sexo es  :</b> ". $_POST['hm']. "<br />";
+echo "<b>La direccion es:</b> ". $_POST['direccion']. "<br />";
+echo "<b>Es estudiante: </b>". $_POST['estudiante']. "<br />";
+echo "<b>Nivel de Carrera:</b> ". $_POST['rango']. "<br />";
 
 ?>
