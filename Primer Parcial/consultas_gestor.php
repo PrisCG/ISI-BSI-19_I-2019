67795f187aaf93d0f0eef553af8456bf8804fdf5
PrisCG <prisgudi17@hotmail.com>
2019-03-10 20:59:15 -0600

    Create consultas_gestor.php

---
+++ Primer Parcial/consultas_gestor.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CONSULTAS</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+      <h1>CONSULTAS</h1>
+	<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" href="consulta_emp_estado.php">Consultas de empleados por estado</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="consulta_emp_departamento.php">Consultas de empleados por departamento</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="acciones_personal_tipo.php">Consulta de acciones por tipo</a>
+  </li>
+</ul>
+<a href="menu1.php" class="btn btn-success pull-right">Volver</a>
+</body>
+</html>
