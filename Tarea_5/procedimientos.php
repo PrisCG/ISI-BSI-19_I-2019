bf0193ef3c755ea5087e826a65746e19df9c0743
PrisCG <prisgudi17@hotmail.com>
2019-04-03 22:52:16 -0600

    Create procedimientos.php

---
+++ Tarea_5/procedimientos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CONSULTAS</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+      <h1>CONSULTAS</h1>
+	<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" href="incre_10.php">Incrementar 10%</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Procedimiento almacenado por Editorial y valor de Incremento</a>
+  </li>
+</ul>
+<a href="index.php" class="btn btn-success pull-right">Volver</a>
+</body>
+</html>
